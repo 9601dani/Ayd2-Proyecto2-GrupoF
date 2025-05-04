5c5f34401a710a839d674a563fa92ed0368db9e9
Isaac03483 <levihernandez2501@gmail.com>
2025-05-03 20:44:17 -0600

    feat:created login component, userService in front app and logic to authenticate user with username or email in backend

--- app-frontend/src/app/components/commons/home/home.component.ts
+++ app-frontend/src/app/components/commons/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TemplateComponent} from '../template/template.component';
 import {ModalComponent} from '../modal/modal.component';
+import {LoginComponent} from '../login/login.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, TemplateComponent, ModalComponent],
+  imports: [CommonModule, TemplateComponent, ModalComponent, LoginComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
