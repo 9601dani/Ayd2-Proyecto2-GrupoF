a1db1e5328cad1b5802ffb32a3e34246a8ee5211
Isaac03483 <levihernandez2501@gmail.com>
2025-05-11 23:20:45 -0600

    feat: logic for comments created

---
+++ app-backend/ms-project/src/main/java/com/codenbugs/ms_project/exceptions/comment/CommentNotCreatedException.java
@@ -0,0 +1,11 @@
+package com.codenbugs.ms_project.exceptions.comment;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class CommentNotCreatedException extends Exception {
+    public CommentNotCreatedException(String message) {
+        super(message);
+    }
+}
